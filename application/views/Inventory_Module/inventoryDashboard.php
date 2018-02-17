68346f48181c5b8d29549c0dd9163150e073f021
Leyonnie <leogascon05@gmail.com>
2018-02-17 15:35:24 +0800

    removed .php in href.

--- application/views/Inventory_Module/inventoryDashboard.php
+++ application/views/Inventory_Module/inventoryDashboard.php
@@ -53,43 +53,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         </a>
                     </li>
                     <li>
-                        <a href="<?php echo base_url(); ?>inventoryStocks.php">
+                        <a href="<?php echo base_url(); ?>inventoryStocks">
                             <i class="material-icons">assessment</i>
                             <p>Inventory Stocks</p>
                         </a>
                     </li>
                     <li>
-                        <a href="<?php echo base_url(); ?>inventoryInventoryReport.php">
+                        <a href="<?php echo base_url(); ?>inventoryInventoryReport">
                             <i class="material-icons">content_paste</i>
                             <p>Inventory Report</p>
                         </a>
                     </li>
                     <li>
-                        <a href="<?php echo base_url(); ?>inventoryPOAdd.php">
+                        <a href="<?php echo base_url(); ?>inventoryPOAdd">
                             <i class="material-icons">shopping cart</i>
                             <p>Purchase Order</p>
                         </a>
                     </li>
                     <li>
-                        <a href="<?php echo base_url(); ?>inventoryOutRawCoffee.php">
+                        <a href="<?php echo base_url(); ?>inventoryOutRawCoffee">
                             <i class="material-icons">reply</i>
                             <p>Inventory Out</p>
                         </a>
                     </li>
                     <li>
-                        <a href="<?php echo base_url(); ?>inventoryItemList.php">
+                        <a href="<?php echo base_url(); ?>inventoryItemList">
                             <i class="material-icons">storage</i>
                             <p>Items</p>
                         </a>
                     </li>
                     <li>
-                        <a href="<?php echo base_url(); ?>inventoryReturnsList.php">
+                        <a href="<?php echo base_url(); ?>inventoryReturnsList">
                             <i class="material-icons">input</i>
                             <p>Returns</p>
                         </a>
                     </li>
                     <li>
-                        <a href="<?php echo base_url(); ?>inventorySamplesList.php">
+                        <a href="<?php echo base_url(); ?>inventorySamplesList">
                             <i class="material-icons">dvr</i>
                             <p>Samples</p>
                         </a>
@@ -121,13 +121,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 </a>
                                 <ul class="dropdown-menu">
                                     <li>
-                                        <a href="<?php echo base_url(); ?>inventoryUser.php">User Profile</a>
+                                        <a href="<?php echo base_url(); ?>inventoryUser">User Profile</a>
                                     </li>
                                     <li>
-                                        <a href="<?php echo base_url(); ?>inventoryChangePassword.php">Change Password</a>
+                                        <a href="<?php echo base_url(); ?>inventoryChangePassword">Change Password</a>
                                     </li>
                                     <li>
-                                        <a href="<?php echo base_url(); ?>inventoryActivityLogs.php">Activity Logs</a>
+                                        <a href="<?php echo base_url(); ?>inventoryActivityLogs">Activity Logs</a>
                                     </li>
                                     <li>
                                         <a href="#">Logout</a>
@@ -152,7 +152,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                         <small>grams</small>
                                     </h3>
                                 </div>
-                                <a href="<?php echo base_url(); ?>inventoryStocks.php">
+                                <a href="<?php echo base_url(); ?>inventoryStocks">
                                 <div class="card-footer">
                                     <div class="stats">
                                         <i class="material-icons">date_range</i> Details
@@ -173,7 +173,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                         <small>pieces</small>
                                     </h3>
                                 </div>
-                                <a href="<?php echo base_url(); ?>inventoryPackaging.php">
+                                <a href="<?php echo base_url(); ?>inventoryPackaging">
                                 <div class="card-footer">
                                     <div class="stats">
                                         <i class="material-icons">date_range</i> Details
@@ -193,7 +193,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                     <small>pieces</small>
                                     </h3>
                                 </div>
-                                <a href="<?php echo base_url(); ?>inventoryStickers.php">
+                                <a href="<?php echo base_url(); ?>inventoryStickers">
                                 <div class="card-footer">
                                     <div class="stats">
                                         <i class="material-icons">date_range</i> Details
@@ -212,7 +212,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                     <small>pieces</small>
                                     </h3>
                                 </div>
-                                <a href="<?php echo base_url(); ?>inventoryMachines.php">
+                                <a href="<?php echo base_url(); ?>inventoryMachines">
                                 <div class="card-footer">
                                     <div class="stats">
                                         <i class="material-icons">date_range</i> Details
@@ -245,14 +245,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                                     </li>
                                                     <span></span>
                                                     <li class="active center">
-                                                        <a href="<?php echo base_url(); ?>inventoryDashboard.php">
+                                                        <a href="<?php echo base_url(); ?>inventoryDashboard">
                                                             Date In
                                                             <div class="ripple-container"></div>
                                                         </a>
                                                     </li>
                                                     <span></span>
                                                     <li class="">
-                                                        <a href="<?php echo base_url(); ?>inventoryDashboardDateOut.php">
+                                                        <a href="<?php echo base_url(); ?>inventoryDashboardDateOut">
                                                             Date Out
                                                             <div class="ripple-container"></div>
                                                         </a>
