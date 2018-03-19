fbeb7caf374a3e860cde424788fbad66fe1c2892
tincaguioa <tinzcaguioa1997@gmail.com>
2018-03-19 00:12:11 -0700

    Functionality for deliveries; new db version7 Signed-off-by: tincaguioa <tinzcaguioa1997@gmail.com>

--- application/models/SalesDelivery_model.php
+++ application/models/SalesDelivery_model.php
@@ -6,12 +6,12 @@
 		}
 		
 		public function get_delivery_list(){
-			$query = $this->db->query("SELECT * FROM contracted_po NATURAL JOIN client_delivery NATURAL JOIN contracted_client NATURAL JOIN coffee_blend NATURAL JOIN packaging");
+			$query = $this->db->query("SELECT * FROM contracted_po NATURAL JOIN contracted_client NATURAL JOIN coffee_blend NATURAL JOIN packaging WHERE delivery_stat = 'pending'");
 			return $query->result();
 			
 		}
 		public function get_delivered(){
-			$query = $this->db->query("SELECT * FROM contracted_po NATURAL JOIN client_delivery NATURAL JOIN contracted_client NATURAL JOIN coffee_blend NATURAL JOIN packaging NATURAL JOIN payment_contracted WHERE  payment_remarks = 'unpaid'");
+			$query = $this->db->query("SELECT * FROM contracted_po NATURAL JOIN client_delivery NATURAL JOIN contracted_client NATURAL JOIN coffee_blend NATURAL JOIN packaging WHERE delivery_stat = 'delivered' AND payment_remarks='unpaid' AND client_delivery.return='No'");
 			return $query->result();
 			
 		}
@@ -19,6 +19,42 @@
 			$query = $this->db->query("SELECT * FROM payment_contracted NATURAL JOIN client_delivery NATURAL JOIN contracted_client WHERE  payment_remarks = 'paid'");
 			return $query->result();
 			
+		}
+
+		function insert_data($data){ 
+			$this->db->insert('client_delivery', $data);
+		}
+
+		function update($deliver, $po_id){
+			$data1 = array(
+		        'delivery_stat' => $deliver
+			);
+
+			$this->db->where('contractPO_id', $po_id);
+			$this->db->update('contracted_po', $data1);
+		}
+		function insert_dataA($dataA){ 
+			$this->db->insert('client_coffreturn', $dataA);
+		}
+		function updateA($return, $dr){
+			$data2 = array(
+		        'return' => $return
+			);
+
+			$this->db->where('client_dr', $dr);
+			$this->db->update('client_delivery', $data2);
+		}
+
+		function insert_dataB($dataB){ 
+			$this->db->insert('payment_contracted', $dataB);
+		}
+		function updateB($pay, $dr){
+			$dataB = array(
+		        'payment_remarks' => $pay
+			);
+
+			$this->db->where('client_dr', $dr);
+			$this->db->update('client_delivery', $dataB);
 		}
 	}
 
