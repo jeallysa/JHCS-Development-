a76deecdb87f057c14aa670965987b8d9179dd1a
rommelarciosa <rommeljohnarciosa@gmail.com>
2018-03-29 14:49:44 +0800

    edit inventory and samples list

--- application/models/InventorySamplesList_Model.php
+++ application/models/InventorySamplesList_Model.php
@@ -13,23 +13,23 @@ class InventorySamplesList_Model extends CI_model
 	}
 
 	function fetch_data(){
-		$query = $this->db->query("SELECT client_dr, sample_date, sample_type, sample_recipient, package_type, package_size, sticker, coff_returnAction FROM jhcs.sample NATURAL JOIN client_coffreturn NATURAL JOIN packaging INNER JOIN sticker on sample.sticker_id=sticker.sticker_id WHERE coff_returnAction='sample';");
+		$query = $this->db->query("SELECT client_dr, sample_date, sample_type, sample_recipient, packaging.name AS packagingName, packaging.type AS packagingType, sticker.name AS stickerName FROM sample NATURAL JOIN client_coffreturn NATURAL JOIN packaging INNER JOIN sticker on sample.sticker_id=sticker.sticker_id WHERE coff_returnAction='sample';");
 		return $query->result();
 	}
 	function fetch_data2(){
-		$query = $this->db->query("SELECT sample_date, sample_type, sample_recipient, package_type, package_size, sticker FROM jhcs.sample NATURAL JOIN packaging INNER JOIN sticker on sample.sticker_id=sticker.sticker_id WHERE client_coffReturnID IS NULL;");
+		$query = $this->db->query("SELECT sample_date, sample_type, sample_recipient, packaging.name AS packagingName, packaging.type AS packagingType, sticker.name AS stickerName FROM sample NATURAL JOIN packaging INNER JOIN sticker on sample.sticker_id=sticker.sticker_id WHERE client_coffReturnID IS NULL;");
 		return $query->result();
 	}
 	public function get_drnumber(){
-		$query = $this->db->query("SELECT client_coffReturnID, client_delivery.client_dr FROM jhcs.client_coffreturn INNER JOIN client_delivery ON client_coffreturn.client_deliveryID = client_delivery.client_deliveryID WHERE lower(coff_returnAction) = 'sample';");
+		$query = $this->db->query("SELECT * FROM client_coffreturn INNER JOIN client_delivery ON client_coffreturn.client_deliveryID = client_delivery.client_deliveryID WHERE lower(coff_returnAction) = 'sample';");
 		return $query->result();	
 	}
 	public function get_packaging(){
-		$query = $this->db->query("SELECT package_id, CONCAT(package_type,' ',package_size) AS package FROM jhcs.packaging WHERE pack_activation = '1';");
+		$query = $this->db->query("SELECT package_id, CONCAT(name,' ',type) AS package FROM packaging WHERE pack_activation = '1';");
 		return $query->result();	
 	}
 	public function get_sticker(){
-		$query = $this->db->query("SELECT sticker_id, sticker FROM jhcs.sticker WHERE sticker_activation = '1';");
+		$query = $this->db->query("SELECT * FROM sticker WHERE sticker_activation = '1';");
 		return $query->result();	
 	}
 	function insert_data($data){ 
--- application/views/Inventory_Module/inventoryReturnsList.php
+++ application/views/Inventory_Module/inventoryReturnsList.php
@@ -261,7 +261,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                                     <td><?php echo $row->sup_returnDate; ?></td>
                                                     <td><?php echo $row->sup_company; ?></td>
                                                     <td><?php echo number_format($row->sup_returnQty); ?> g</td>
-                                                    <td><?php echo $row->raw_coffee; ?></td>
+                                                    <td><?php echo $row->name; ?></td>
                                                     <td><?php echo $row->sup_returnRemarks; ?></td>
                                                 </tr>
                                                 <?php
@@ -321,7 +321,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                                                 <th><b>Quantity</b></th>
                                                                 <th><b>Remarks</b></th>
                                                                 <th><b>Action Taken</b></th>
-                                                                <th><b>Status</b></th>
                                                             </tr>
                                                         </thead>
                                                         <tbody>
@@ -331,7 +330,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                                     ?>
                                                             <tr>
                                                                 <td><?php echo $row->client_machReturnID; ?></td>
-                                                                <td><?php echo $row->client_dr; ?></td>
+                                                                <td> </td>
                                                                 <td><?php echo $row->mach_returnDate; ?></td>
                                                                 <td><?php echo $row->client_company; ?></td>
                                                                 <td><?php echo $row->machine; ?></td>
@@ -396,7 +395,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
                                                     foreach($data5['get_coffee'] as $row)
                                                     { 
-                                                        echo '<option value="'.$row->raw_id.'">'.$row->raw_coffee.'</option>';
+                                                        echo '<option value="'.$row->raw_id.'">'.$row->name.'</option>';
                                                     }
                                                  ?>
                                             </select>
