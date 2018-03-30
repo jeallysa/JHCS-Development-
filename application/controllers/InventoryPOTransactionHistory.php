e4a241d262fd40c2ea886f5972b0e3796dd6233f
rommelarciosa <rommeljohnarciosa@gmail.com>
2018-03-31 02:44:54 +0800

    PO and DB ver 25

--- application/controllers/InventoryPOTransactionHistory.php
+++ application/controllers/InventoryPOTransactionHistory.php
@@ -5,16 +5,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	{
 		function __construct(){
 			parent::__construct();
+            
+             $this->load->model('inventoryPOTransactionHistory_model');
+             $this->load->model('notification_model');
 		}
 		
 		public function index()
 		{ 
-            $this->load->view('layout/header');
-            $this->load->model('inventoryPOTransactionHistory_model');
+      //      $this->load->view('layout/header');
+           
             
             
             $data['Transactions'] = $this->inventoryPOTransactionHistory_model ->retrieveTransactions();
-            
+            $data['reorder'] = $this->notification_model->reorder();
             
             
 			$this->load->view('inventoryPOTransactionHistory' , $data);
