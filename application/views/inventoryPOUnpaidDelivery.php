975401bd188fe14b4eb4a4bd525b3209881a6e25
jomarilester <2131506@slu.edu.ph>
2018-04-02 00:56:46 +0800

    edit notification in purchase order
    Signed-off-by: jomarilester <2131506@slu.edu.ph>

--- application/views/inventoryPOUnpaidDelivery.php
+++ application/views/inventoryPOUnpaidDelivery.php
@@ -11,7 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url(); ?>assets/img/apple-icon.png"/>
     <link rel="icon" type="image/png" href="<?php echo base_url(); ?>assets/img/favicon.png"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>Inventory Stocks</title>
+    <title>Purchase Order</title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
     <!-- Bootstrap core CSS     -->
@@ -29,6 +29,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <style>
 .title {
     font-size: large;
+    padding-top: 15px;
 
 }
 </style>
@@ -88,13 +89,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                             <p>Returns</p>
                         </a>
                     </li>
-                    <li>
-                        <a href="<?php echo base_url(); ?>inventorySamplesList">
-                            <i class="material-icons">dvr</i>
-                            <p>Samples</p>
-                        </a>
-                    </li>
-                    
                 </ul>
             </div>
         </div>
@@ -103,24 +97,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <div class="main-panel">
             <nav class="navbar navbar-transparent navbar-absolute">
                 <div class="container-fluid">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                    </div>
-                    
-                    
-                    
-                    
-                    
-                    
                     <div class="collapse navbar-collapse">
                         <ul class="nav navbar-nav navbar-right">
-                            <li class="dropdown">
-                                <li>
+                            
+                                <li id="nameheader">
                                     <?php $username = $this->session->userdata('username') ?>
                                 
                                 <?php
@@ -133,6 +113,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                             }
                                         ?>
                                 </li>
+                           
+                            <li>
                                 <a href="#pablo" class="dropdown-toggle" data-toggle="dropdown">
                                         <i class="material-icons">person</i>
                                         <p class="hidden-lg hidden-md">Profile</p>
@@ -151,14 +133,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                         <a href="<?php echo base_url('Login/logout');  ?>">Logout</a>
                                     </li>
                                 </ul>
-                            
+                            </li>
                                
-          <!------------------                                          NOTIFICATION                    ---------------------------------->           
+       <!------------------                                          NOTIFICATION                    ---------------------------------->           
                             
-                            
+                            <li>
                             
                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                        <i class="material-icons">shopping_basket</i>
+                                        <i class="material-icons">announcement</i>
                                         <p class="hidden-lg hidden-md">Profile</p>
                                        <span class="label-count" style='background-color: #f44336;'> <?php 
                                            
@@ -175,13 +157,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                             
                             
                             
+                            
                                 <ul class="dropdown-menu">
                                     
                                    <?php 
                                  for($i = 0; $i <= 3 ;$i++){
                                      if(!empty($reorder[$i])){
                                           foreach($reorder[$i] as $object){
-                                            echo   '<li>' . $object->name . "     " . $object->type. '</li>';
+                                            echo   '<li><a href="inventoryStocks">' . $object->name . "     " . $object->type. ' now drops below the re-order level</a></li>';
                                                  
                                              }
                                       }
@@ -189,36 +172,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                     ?>
                                    
                                 </ul>
-            
-         <!------------------                                          NOTIFICATION                    ---------------------------------->                               
+                            
+                            </li>
                             
                             
                             
-                             </li>
-                 
-                        
-                        
+    <!------------------                                          NOTIFICATION                    ---------------------------------->           
+
                         
                         </ul>
                     </div>
                 
                 </div>
             </nav>
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
             <div class="content">
                 <div class="container-fluid">
